a99262b4412c823536ee99b16ca4ce9c46611bd0
NazmusSakib-tech <sakibkhancse09@gmail.com>
2022-01-24 16:26:13 +0600

    single update and single find API

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const Student = require('./models/students');
+const chartOfAccount = require('./models/chartOfAccount');
 // const Course = require('./models/courses');
 require('./db/conn');
 
@@ -12,12 +12,58 @@ app.use(express.json());
 app.use((err, req, res, next) => {
     console.log(err.message);
 })
-// create a new student 
+// create a new COA 
 app.get('/', (req, res) => {
     res.send('test chart of Accounts ');
 });
 
+// insert chart 
+try {
+    app.post('/chart', async (req, res) => {
+        const chart = new chartOfAccount(req.body);
+        console.log(req.body);
+        const createChart = await chart.save();
+        res.status(201).send(createChart);
+    })
+} catch (err) {
+    res.status(400).send(err);
+}
 
+// Find all chart 
+
+try {
+    app.get('/chart', async (req, res) => {
+        const chartData = await chartOfAccount.find();
+        res.send(chartData);
+    })
+} catch (err) {
+    res.send(err);
+}
+
+// single Chart find
+
+try {
+    app.get('/chart/:id', async (req, res) => {
+        const _id = req.params.id;
+        console.log(_id);
+        const singleChartData = await chartOfAccount.findOne({ _id: _id });
+        res.send(singleChartData);
+    })
+} catch (err) {
+    res.send(err);
+}
+
+// single Chart Update
+try {
+    app.patch('/chart/:id', async (req, res) => {
+        const _id = req.params.id;
+        console.log(_id);
+        const singleChartDataUpdate = await chartOfAccount.findByIdAndUpdate(_id, req.body);
+        res.send(singleChartDataUpdate);
+    })
+} catch (err) {
+    res.status(400).send(err);
+}
 
 
 
--- models/chartOfAccount.js
+++ models/chartOfAccount.js
@@ -1,8 +1,52 @@
 const mongoose = require('mongoose');
 
 
-const chartOfAccountsSchema = new mongoose.Schema({
+const chartOfAccountSchema = new mongoose.Schema({
+    c_id: {
+        type: String,
 
+    },
+    class_code: {
+        type: String,
 
+    },
+    class_name: {
+        type: String,
+
+    },
+    class_type: {
+        type: String,
+        unique: true
+    },
+    active_status: {
+        type: Boolean
+    },
+    group: [
+        {
+            group_code:
+            {
+                type: Number
+            },
+            group_name:
+            {
+                type: String
+            },
+
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 
 })
+
+//we will create a new collection
+
+const chartOfAccount = new mongoose.model('chartOfAccount', chartOfAccountSchema);
+
+module.exports = chartOfAccount;
